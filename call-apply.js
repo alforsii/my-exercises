ac66ed84f738fa4a73e2d8c8bf6d0e91f8db0462
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-12-24 08:58:16 -0500

    add bind method

--- call-apply.js
+++ call-apply.js
@@ -1,11 +1,54 @@
-//5.Get arguments without passing it to a function
+//call() method
+//Get arguments without passing it to a function
 let argsToArr = function() {
-  console.log(arguments);
+  console.log(arguments); //result: Arguments(3) [1, 2, 3, callee: ƒ, Symbol(Symbol.iterator): ƒ]
 };
-argsToArr(1, 2, 3); //result: Arguments(3) [1, 2, 3, callee: ƒ, Symbol(Symbol.iterator): ƒ]
-//to convert we can use array method
+argsToArr(1, 2, 3);
+
+//to convert arguments to an array we can use array method slice()
 argsToArr = function() {
   console.log([].slice.call(arguments)); // result [1,2,3]
   return [].slice.call(arguments);
 };
 argsToArr(1, 2, 3);
+
+//Another example for call method
+let animal = function(legs) {
+  this.legs = legs;
+};
+let cat = function(legs, isDomestic) {
+  animal.call(this, legs); //basically === to this.legs=legs; by using call() get argument from another object
+  this.isDomestic = isDomestic;
+};
+let lion = new cat(4, false);
+console.log(lion); //result: cat {legs: 4, isDomestic: false}
+
+//apply() method
+//apply method unlike call method, it converts array into regular
+//lets find min of numbers
+console.log(Math.min(1, 2, 3)); //result: 1
+//what if we want to find min of given array numbers
+let numArray = [1, 2, 3];
+//we can use spread operator
+Math.min(...numArray);
+console.log(Math.min(...numArray)); // 1
+//or another way we can apply method
+Math.min.apply(null, numArray);
+console.log(Math.min.apply(null, numArray)); // 1
+
+//bind() method
+let button = function(content) {
+  this.content = content;
+};
+
+button.prototype.click = function() {
+  console.log(`${this.content} clicked`);
+};
+let newButton = new button('add');
+newButton.click(); //add clicked
+let looseClick = newButton.click;
+// looseClick = newButton.click();//Uncaught TypeError: looseClick is not a function
+looseClick(); // undefined clicked
+//so in this situation we can use bind()
+let boundClick = newButton.click.bind(newButton);
+boundClick(); // add clicked
