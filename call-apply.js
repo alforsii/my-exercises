f8a9fe20a3a5a6afd0901e620dfe324deebb4010
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-12-24 09:20:37 -0500

    another example

--- call-apply.js
+++ call-apply.js
@@ -1,4 +1,4 @@
-//call() method
+//1.call() method
 //Get arguments without passing it to a function
 let argsToArr = function() {
   console.log(arguments); //result: Arguments(3) [1, 2, 3, callee: ƒ, Symbol(Symbol.iterator): ƒ]
@@ -22,8 +22,8 @@ let cat = function(legs, isDomestic) {
 };
 let lion = new cat(4, false);
 console.log(lion); //result: cat {legs: 4, isDomestic: false}
-
-//apply() method
+// ---------------------------------------------------------
+//2.apply() method
 //apply method unlike call method, it converts array into regular
 //lets find min of numbers
 console.log(Math.min(1, 2, 3)); //result: 1
@@ -35,8 +35,8 @@ console.log(Math.min(...numArray)); // 1
 //or another way we can apply method
 Math.min.apply(null, numArray);
 console.log(Math.min.apply(null, numArray)); // 1
-
-//bind() method
+// ----------------------------------------------------
+//3.bind() method
 let button = function(content) {
   this.content = content;
 };
@@ -52,3 +52,75 @@ looseClick(); // undefined clicked
 //so in this situation we can use bind()
 let boundClick = newButton.click.bind(newButton);
 boundClick(); // add clicked
+// ===================================================
+//another example for bind()
+let myObj = {
+  asyncGet(cb) {
+    cb();
+  },
+  parse() {
+    console.log('parse called');
+  },
+  render() {
+    this.asyncGet(function() {
+      this.parse();
+      //Uncaught TypeError: this.parse is not defined
+      //because 'this' in this function block scope it has it's own local 'this'
+      //and cannot find parse() within this function()
+    });
+  },
+};
+// myObj.render();
+//a)one solution: using arrow function instead function(),which makes 'this' global
+myObj = {
+  asyncGet(cb) {
+    cb();
+  },
+  parse() {
+    console.log('parse called');
+  },
+  render() {
+    this.asyncGet(() => {
+      //solution arrow =>
+      this.parse();
+    });
+  },
+};
+
+// myObj.render();
+// b).Switch 'this' keyword with 'that'
+myObj = {
+  asyncGet(cb) {
+    cb();
+  },
+  parse() {
+    console.log('parse called');
+  },
+  render() {
+    let that = this; //solution
+    this.asyncGet(function() {
+      that.parse();
+    });
+  },
+};
+
+// myObj.render();
+
+// c)Using bind() - bind outer objects 'this' with local 'this':
+myObj = {
+  asyncGet(cb) {
+    cb();
+  },
+  parse() {
+    console.log('parse called');
+  },
+  render() {
+    this.asyncGet(
+      function() {
+        this.parse();
+      }.bind(this)
+    );
+  },
+};
+
+myObj.render();
